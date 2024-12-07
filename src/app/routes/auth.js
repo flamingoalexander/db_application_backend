c106344305d13238df110775333a92ed9c86bc48
Alexander Vokin <setvvgamer@gmail.com>
2024-12-07 22:24:20 +0800

    send user rights to frontend

--- src/app/routes/auth.js
+++ src/app/routes/auth.js
@@ -3,26 +3,38 @@ const { DBQuery } = require('@/services/database')
 const { generateRandomString } = require('@/utils/ctypto')
 
 const router = express.Router();
-
+const TABLE_RIGHTS = {
+    admin: ['employees', 'phone_numbers', 'employees_and_departments',
+        'disciplines', 'disciplines_and_specialties', 'specialties',
+        'disciplines_and_employees', 'departments' ],
+    secretary: ['disciplines_and_employees'],
+    ED: ['disciplines', 'disciplines_and_specialties', 'specialties'],
+    HR: ['employees', 'phone_numbers', 'employees_and_departments'],
+}
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     console.log(password, username);
     try {
-        const userResult = await DBQuery(
-            'SELECT passwd, user_id FROM users WHERE username = $1 LIMIT 1;',
+        const result = await DBQuery(
+            'SELECT passwd, user_id, rights FROM users WHERE username = $1 LIMIT 1;',
             [username]
         );
-        if (userResult.rowCount > 0) {
-            const userPasswd = userResult.rows[0].passwd;
+        if (result.rowCount > 0) {
+            const user = result.rows[0];
+            const userPasswd = user.passwd;
             if (userPasswd === password) {
                 const sessionToken = generateRandomString();
-                const user_id = userResult.rows[0].user_id;
+                const user_id = user.user_id;
+                const userRights = user.rights;
+                console.log(TABLE_RIGHTS[userRights]);
                 await DBQuery(
                     `INSERT INTO active_sessions (user_id, session_token) VALUES (${user_id}, '${sessionToken}');`
                 );
+                console.log(userRights)
                 res.status(200).json({
                     message: 'Login successful',
                     token: sessionToken,
+                    rights: TABLE_RIGHTS[userRights]
                 });
             } else {
                 console.log('wrong paswd');
