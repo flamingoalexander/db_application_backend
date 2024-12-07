9ee1db1ec291ddb531190ce921c4b89f29fd876d
Alexander Vokin <setvvgamer@gmail.com>
2024-12-07 18:10:14 +0800

    add api for departments

---
+++ src/app/routes/tables/departments.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const { DBQuery } = require('@/services/database');
+
+const router = express.Router();
+
+const tableName = 'departments';
+
+router.put(`/table/${tableName}`, async (req, res) => {
+    const department = req.body.department
+    try {
+        DBQuery(`INSERT INTO ${tableName} (
+            department_name,
+            department_type
+        ) VALUES (
+             '${department.department_name}', 
+             '${department.department_type}');
+        `)
+        res.status(200).json({message:'Success'})
+    } catch (err) {
+        res.status(500).json({message:err.message});
+    }
+});
+
+router.patch(`/table/${tableName}`, async (req, res) => {
+    const department = req.body.department
+    try {
+        DBQuery(`UPDATE ${tableName}
+            SET
+                department_name = '${department.department_name}',
+                department_type = '${department.department_type}'
+            WHERE
+                department_id = ${department.department_id}; 
+        `)
+        res.status(200).json({message:'Success'})
+        console.log('success patch' + department);
+    } catch (err) {
+        res.status(500).json({message:err.message});
+        console.log(err)
+    }
+});
+
+router.delete(`/table/${tableName}`, async (req, res) => {
+    const department_id = req.body.department.department_id
+    try {
+        DBQuery(`DELETE FROM ${tableName} WHERE department_id = ${department_id};`)
+        res.status(200).json({message:'Success'})
+        console.log('success delete' + department_id);
+    } catch (err) {
+        res.status(500).json({message:err.message});
+    }
+});
+module.exports = router;
