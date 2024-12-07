c1503e5545f169724d179ff9794d7ef988e62f0c
Alexander Vokin <setvvgamer@gmail.com>
2024-12-07 18:24:18 +0800

    include api

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const cors = require('@/app/middlewares/cors');
 const authMiddleware = require('@/app/middlewares/auth');
 const authRoutes = require('@/app/routes/auth');
 const employeesApi = require('@/app/routes/tables/employees')
+const disciplinesApi = require('@/app/routes/tables/disciplines')
+const departmentsApi = require('@/app/routes/tables/departments')
+const specialtiesApi = require('@/app/routes/tables/specialties')
+
 const queryRoutes = require('@/app/routes/query');
 
 const app = express();
@@ -18,6 +22,9 @@ app.use('/api', authRoutes);
 
 app.use('/api', authMiddleware);
 app.use('/api', employeesApi);
+app.use('/api', disciplinesApi);
+app.use('/api', departmentsApi);
+app.use('/api', specialtiesApi);
 app.use('/api', queryRoutes);
 
 module.exports = app;
