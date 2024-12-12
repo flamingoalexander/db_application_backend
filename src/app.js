0275ce893ef69ac39c61a5023fcd1cd8682c7b5f
Alexander Vokin <setvvgamer@gmail.com>
2024-12-12 20:32:35 +0800

    include api

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const disciplinesApi = require('@/app/routes/tables/disciplines')
 const departmentsApi = require('@/app/routes/tables/departments')
 const specialtiesApi = require('@/app/routes/tables/specialties')
 const phoneNumbersApi = require('@/app/routes/tables/phone_numbers')
+const disciplines_and_employeesApi = require('@/app/routes/tables/disciplines_and_employees')
+const disciplines_and_specialtiesApi = require('@/app/routes/tables/phone_numbers')
+const employees_and_departmentsApi = require('@/app/routes/tables/phone_numbers')
 const queryRoutes = require('@/app/routes/query');
 
 const app = express();
@@ -26,6 +29,11 @@ app.use('/api', disciplinesApi);
 app.use('/api', departmentsApi);
 app.use('/api', specialtiesApi);
 app.use('/api', phoneNumbersApi);
+
+app.use('/api', disciplines_and_employeesApi);
+app.use('/api', disciplines_and_specialtiesApi);
+app.use('/api', employees_and_departmentsApi);
+
 app.use('/api', queryRoutes);
 
 module.exports = app;
